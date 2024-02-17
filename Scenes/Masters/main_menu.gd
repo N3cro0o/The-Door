7cfa9da162c065cbc69980c10ecf561634bce8ad
N3cro0o <calko.n3cro0o@gmail.com>
2024-02-17 21:14:36 +0100

    added main menu scene

---
+++ Scenes/Masters/main_menu.gd
@@ -0,0 +1,33 @@
+extends Sprite2D
+class_name MainMenu
+
+# Exported variables
+
+# Variables
+@onready var go_back_bttn := $Control/MarginContainer/HBoxContainer/Buttons/Continue
+@onready var quit_bttn := $Control/MarginContainer/HBoxContainer/Buttons/Quit
+@onready var master_sound_slider := $Control/MarginContainer/HBoxContainer/Sliders/Label/MasterS
+@onready var mr_w_slider := $Control/MarginContainer/HBoxContainer/Sliders/Label2/MrW
+@onready var master_bus := AudioServer.get_bus_index("Master")
+@onready var player := $SamplePlayer
+
+# Signals
+signal end_menu
+signal end_game
+
+# Methods
+
+func _on_continue():
+	end_menu.emit()
+
+func _on_quit():
+	end_game.emit()
+
+func _on_sound_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+
+func _on_sound_drag_ended(value_changed):
+	player.play()
+
+func _on_mr_w(value):
+	pass # Replace with function body.
